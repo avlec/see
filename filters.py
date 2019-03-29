df65440338a825422367672105ed91f6de7811b1
Alec Cox <avlec@uvic.ca>
2019-03-29 12:09:46 -0700

    Moved functionality from constructor to add_filter, formatted error/log messages

--- filters.py
+++ filters.py
@@ -8,10 +8,10 @@ class FilterBase:
             self.inputfile = open(filename)
         except FileNotFoundError:
             self.inputfile = None
-            print("No file found.")
+            print("ERR: file found.")
         except TypeError:
             self.inputfile = None
-            print("No file specified.")
+            print("ERR: No file specified.")
     def generate_warn(self):
         return { 'warning' : 'No defined behaviour.'}
 
@@ -33,6 +33,9 @@ class LineCountFilter(FilterBase):
         }
 
 class FilterAgregator:
+	"""
+	Each filter is stored as (str, filter)
+	"""
     filters = []
 
     filter_results = {}
@@ -45,43 +48,35 @@ class FilterAgregator:
     """
     def __init__(self, filters=None):
         if filters == None:
-            print("No filters provided to agregator, expecting them to be added later.")
+            print("LOG: No filters provided to agregator, expecting them to be added later.")
             return
         # If list, process each item in the list.
         if isinstance(filters, list):
             for filter in filters:
-                if isinstance(filter, tuple):
-                    if isinstance(filter[1], FilterBase) and isinstance(filter[0], str):
-                        print("Adding filter {0}".format(filter[0]))
-                        self.filters.append(filter)
-                    elif isinstance(filter[0], FilterBase) and isinstance(filter[1], str):
-                        print("Adding filter {0}".format(filter[1]))
-                        self.filters.append(tuple(reversed(filter)))
-                    else:
-                        print("Error filter agregator, type isn't instance of FilterBase")
-                else:
-                    if isinstance(filter, FilterBase):
-                        self.filters.append((filter.__class__.__name__, filter))
-                    else:
-                        print("Error filter agregator, type isn't instance of FilterBase")
+				self.add_filter(filter)
         # If not a list, is tuple or is just a filter?
         else:
-            if isinstance(filters, tuple):
-                print("Adding filter {0}".format(filters[0]))
-                self.filters.append(filters)
-            else:
-                print("Adding filter {0}".format(filters.__class__.__name__))
-                self.filters.append((filters.__class__.__name__, filters))
+            self.add_filter(filters)
 
-    def add_filter(self, filter, filter_name=None):
-        if not isinstance(filter, FilterBase):
-            print("Filter type isn't good.")
-            return
-
-        if filter_name == None:
-            self.filters.append((filter.__class__.__name__, filter))
+    def add_filter(self, filter):
+        # Is this a tuple of class/date or just a class?
+        if isinstance(filter, tuple):
+			# Determine which is the filter and which is the string
+			#	if there isn't a filter string combo error.
+			if isinstance(filter[1], FilterBase) and isinstance(filter[0], str):
+				print("LOG: Adding filter {0}".format(filter[0]))
+				self.filters.append(filter)
+			elif isinstance(filter[0], FilterBase) and isinstance(filter[1], str):
+				print("LOG: Adding filter {0}".format(filter[1]))
+				self.filters.append(tuple(reversed(filter)))
+			else:
+			    print("ERR: filter agregator, type isn't instance of FilterBase")
         else:
-            self.filters.append((filter_name, filter))
+			# Is this a derived filter?
+            if isinstance(filter, FilterBase):
+				self.filters.append((filter.__class__.__name__, filter))
+            else:
+				print("ERR: filter agregator, type isn't instance of FilterBase")
 
     def get_results(self):
         return self.filter_results
@@ -91,7 +86,7 @@ class FilterAgregator:
             if self.filter_results.get(filter[0]) == None:
                 self.filter_results[filter[0]] = filter[1].generate_warn()
             else:
-                print("Conflicting filter names. Filter {0} has been skipped.".format(filter[0]))
+                print("LOG: Conflicting filter names. Filter {0} has been skipped.".format(filter[0]))
 
 def main(argv):
     fltr = LineCountFilter(argv[0])
@@ -104,4 +99,3 @@ def main(argv):
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
